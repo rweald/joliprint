91de5173da44287bec86655b7824c951cbdcc29b
Ryan Weald <ryan@weald.com>
2011-02-11 10:29:15 -0800

    attempt to fix bug with generator

--- lib/generators/joliprint_generator.rb
+++ lib/generators/joliprint_generator.rb
@@ -1,12 +1,10 @@
 module Joliprint
-  module Generators
-    class  InstallGenerator < Rails::Generators::Base
-      desc "Creating View Helper"
-      source_root File.expand_path("../templates", __FILE__)
+  class  InstallGenerator < Rails::Generators::Base
+    desc "Creating View Helper"
+    source_root File.expand_path("../templates", __FILE__)
 
-      def install_view_helper
-        copy_file("_joilprint.html.erb", "lib/templates/erb/_joliprint.html.erb")
-      end
+    def install_view_helper
+      copy_file("_joilprint.html.erb", "lib/templates/erb/_joliprint.html.erb")
     end
   end
 end
