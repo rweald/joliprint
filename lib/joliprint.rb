f4d5722b98391815b9eb1c11d7980c9c22d9d8f4
Ryan Weald <ryan@weald.com>
2011-02-14 21:36:51 -0800

    modify page to render real script

--- lib/joliprint.rb
+++ lib/joliprint.rb
@@ -1,5 +1,9 @@
 module Joliprint
   def self.joliprint_me
-    return "hello world"
+    return %Q[
+<!-- BEGIN joliprint Code -->
+				<script charset='ISO-8859-1' src='http://api.joliprint.com/joliprint/js/joliprint.js' type='text/javascript'></script>
+				<script type='text/javascript'>$joliprint().set("buttonUrl", "http://api.joliprint.com/res/joliprint/img/buttons/default/joliprint-button.png").write();</script>
+				<!-- END joliprint Code -->]
   end
 end
